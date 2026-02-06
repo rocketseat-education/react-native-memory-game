89a68bacaa0da4aed3f8e0efe782bb3261595a82
arthur <arthur.rios007@gmail.com>
2026-02-06 17:07:40 -0300

    Tela de Login - Configurando store de autenticação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,12 @@
+import { useAuthStore } from '@/shared/stores/auth.store'
 import { Redirect } from 'expo-router'
 
 export default function Index() {
+  const { user } = useAuthStore()
+
+  if (user) {
+    return <Redirect href="/(private)/home" />
+  }
+
   return <Redirect href="/(public)/login" />
 }
---
+++ src/shared/stores/auth.store.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+interface AuthStore {
+  user: {
+    id: string
+    name: string
+    createdAt: Date
+  } | null
+  setAuthenticated: (name: string) => void
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      logout: () => {},
+      setAuthenticated: (name: string) => {},
+    }),
+    {
+      name: '@memory-game:auth',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+)
